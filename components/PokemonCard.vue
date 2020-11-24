a3600dff88ab85f2738e8cd3e86ceb72edf4ba3d
Matthieu Lempereur <matthieu.lempereur@betc.com>
2020-11-24 14:08:57 +0100

    Init pokemon list & card

---
+++ components/PokemonCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>{{pokemon.name}}</div>
+</template>
+
+<script lang="ts">
+
+export default {
+    name: "pokemon-card",
+    props: ['pokemon'],
+}
+
+</script>
+
+<style lang="scss">
+</style>
---
+++ components/PokemonList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>{{ data }}</div>
+</template>
+
+<script lang="ts">
+
+export default {
+    name: "pokemon-list",
+    data() { 
+        return {
+            data: []
+        }
+    },
+    mounted() {
+        this.fetchPokemonCollection()
+    },
+    methods: {
+        async fetchPokemonCollection(){
+            const res = await this.$axios.get('https://pokeapi.co/api/v2/pokemon?limit=100')
+            const data = res.data
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
+</style>
