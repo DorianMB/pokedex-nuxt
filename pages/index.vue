c7d9fdee4acd5a1af4641cd8f64153f053e61bf6
Dorian <dorian.marques.2e7@gmail.com>
2020-11-24 13:20:30 +0100

    add starters

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="flex flex-col items-center">
-    <Logo />
-    <div class="pokedex w-full h-64 rounded">
+  <div class="flex flex-col items-center mx-20">
+    <Logo/>
+    <div class="pokedex w-full h-64 rounded-2xl flex justify-center items-center">
+      <div v-for="(pokemon,index) in pokemons" :key="index" class="mx-8 flex flex-col items-center">
+        <img :src="pokemon.sprites.front_default">
+        <span>{{getPokemon(pokemon)}}</span>
+      </div>
     </div>
+    <router-link to="pokemon-list" tag="button" class="bg-white mt-5 rounded-full py-3 px-6 shadow flex justify-center items-center">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 mr-3">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+      </svg>
+      <span>Go to list</span>
+    </router-link>
   </div>
 </template>
 
@@ -10,14 +20,27 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  beforeMount(): void {
-    //this.asyncData();
+  data() {
+    return {
+      pokemons: [null]
+    }
+  },
+  beforeMount() {
+    this.pokemons = [];
+    this.$axios.$get('https://pokeapi.co/api/v2/pokemon/bulbasaur').then(res => {
+      this.pokemons.push(res);
+    });
+    this.$axios.$get('https://pokeapi.co/api/v2/pokemon/squirtle').then(res => {
+      this.pokemons.push(res);
+    });
+    this.$axios.$get('https://pokeapi.co/api/v2/pokemon/charmander').then(res => {
+      this.pokemons.push(res);
+    });
   },
   methods: {
-    async asyncData() {
-      const ip = await this.$axios.$get('http://icanhazip.com');
-      console.log(ip, 'abcd');
-      return { ip }
+    getPokemon(pokemon: any) {
+      console.log(pokemon, 'abcd');
+      return pokemon.name;
     }
   }
 })
@@ -27,6 +50,5 @@ export default Vue.extend({
   .pokedex {
     background-color: white;
     box-shadow: inset 0 2px 4px 0 rgba(0, 0, 0, 0.6);
-    margin: 1rem 5rem;
   }
 </style>
